33a8d3e626c0e4fda9ad3b26f1bc7b2a9002b4eb
shaik Afroz <affrozali9342@gmail.com>
2026-05-23 18:01:46 +0530

    practiced Trap Raining Water

--- dsa.js
+++ dsa.js
@@ -417,4 +417,33 @@ let obj=[
         }
         return result;
     }
-    // console.log(mergeVal(obj))
+    // console.log(mergeVal(obj));
+    // 18 Trap Raining Water 
+    // your code goes here
+function trapRain(height) {
+
+    let left = 0;
+    let right = height.length - 1;
+
+    let leftMax = 0;
+    let rightMax = 0;
+
+    let water = 0;
+
+    while(left < right){
+        if(height[left] < height[right]){
+            leftMax = Math.max(leftMax, height[left]);
+            water += leftMax - height[left];
+            left++;
+        }
+
+        else{
+            rightMax = Math.max(rightMax, height[right]);
+            water += rightMax - height[right];
+            right--;
+        }
+    }
+
+    return water;
+}
+// console.log(trapRain([4,2,0,3,2,5]))
--- polyfill.js
+++ polyfill.js
@@ -1,24 +1,24 @@
-//  1)promise.all polyfill
- p1=Promise.resolve("hello");
-p2=Promise.resolve("hell2");
-p3=Promise.resolve("hell1");
-myPromise([p1,p2,p3]).then((res)=>console.log(res));
-function myPromise(promises){
-    return new Promise((resolve,reject)=>{
-    let result=[];
-    let count=0;
+// //  1)promise.all polyfill
+//  p1=Promise.resolve("hello");
+// p2=Promise.resolve("hell2");
+// p3=Promise.resolve("hell1");
+// myPromise([p1,p2,p3]).then((res)=>console.log(res));
+// function myPromise(promises){
+//     return new Promise((resolve,reject)=>{
+//     let result=[];
+//     let count=0;
     
-        promises.forEach((p,i)=>{
-       Promise.resolve(p).then((res)=>{
-           result[i]=res;
-           count++;
-           if(count===promises.length){
-              resolve(result)
-           }
-       }).catch((res)=>reject(res))
-        })
-    })
-}
+//         promises.forEach((p,i)=>{
+//        Promise.resolve(p).then((res)=>{
+//            result[i]=res;
+//            count++;
+//            if(count===promises.length){
+//               resolve(result)
+//            }
+//        }).catch((res)=>reject(res))
+//         })
+//     })
+// }
 // 2) promise polyfill //
 let p=new myPromise((resolve,reject)=>{
     setTimeout(()=>{
