ff215617805838950a1c61fe0ffb111d3a219d59
shaik Afroz <affrozali9342@gmail.com>
2026-06-13 11:39:40 +0530

    Added Arrays find Dulpicates

--- DSA/Arrays.js
+++ DSA/Arrays.js
@@ -787,4 +787,27 @@ while(left<right){
 }
 return left
 };
-// console.log(peak([1,2,3,1]))
+// console.log(peak([1,2,3,1]));
+
+
+// 33) find dulplicate number 
+function dulplicate(nums) {
+    let slow = nums[0];
+    let fast = nums[0];
+    do {
+        slow = nums[slow];
+        fast = nums[nums[fast]]
+
+    } while (slow !== fast);
+
+    slow = nums[0];
+    while (slow !== fast) {
+        slow = nums[slow];
+        fast = nums[fast];
+    }
+    return slow;
+}
+// console.log(dulplicate([1,3,4,2,2]))
+
+TimeComplexity:O(n);
+spaceComplexity:O(1)
