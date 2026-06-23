39aeb26a349e0b7279fb56bee31d30dedddec367
shaik Afroz <affrozali9342@gmail.com>
2026-06-23 08:44:42 +0530

    Added search in rotated sorted Array 2

--- DSA/Arrays.js
+++ DSA/Arrays.js
@@ -906,4 +906,42 @@ for(let [key,val] of map){
 return s.length
 }
 
-// console.log(Substring("bbaaacbd"))
+// console.log(Substring("bbaaacbd"));
+
+
+// 38) search in rotated sorted Array 2;
+ function search(nums, target) {
+    let left=0;;
+    let right=nums.length-1;
+    while(left<=right){
+      let mid=Math.floor((left+right)/2);
+      if(nums[mid]===target){
+        return true
+      }
+      else if(nums[left]===nums[mid]&& nums[right]===nums[mid]){
+        left++;
+        right--
+      }
+      else if(nums[left]<=nums[mid]){
+        if(nums[left]<=target && target<nums[mid] ){
+            right=mid-1
+        }
+        else{
+            left=mid+1
+        }
+      }
+      else{
+        if(nums[mid]<target && target<=nums[right]){
+            left=mid+1
+        }
+        else
+        {
+            right=mid-1
+        }
+      }
+
+  
+    };
+    return false
+};
+// console.log(search([2,5,6,0,0,1,2],  0))
