11bcd0a6c2689d66579d9d20984e0036ace4111d
shaik Afroz <affrozali9342@gmail.com>
2026-06-28 16:10:16 +0530

    Added fruits into baskets

--- DSA/Arrays.js
+++ DSA/Arrays.js
@@ -945,3 +945,26 @@ return s.length
     return false
 };
 // console.log(search([2,5,6,0,0,1,2],  0))
+
+// 39)fruits into basket 
+ function basket(fruits) {
+
+
+  let map=new Map();
+  let left=0;
+let maxlen=0;
+
+  for(let right=0;right<fruits.length;right++){
+    map.set( fruits[right],(map.get(fruits[right])||0)+1);
+     while(map.size>2){
+        map.set(fruits[left],map.get(fruits[left])-1);
+      if(map.get(fruits[left])===0){
+        map.delete(fruits[left])
+      }
+      left++
+     }
+        maxlen=Math.max(maxlen,right- left+1)
+  }  
+return maxlen
+};
+// console.log(basket([1,2,1]))
--- implement.js
+++ implement.js
@@ -70,3 +70,17 @@ let square=memoized();
 console.log(square(5))
 console.log(square(5))
 console.log(square(6))
+
+
+const fs = require("fs");
+
+const readStream = fs.createReadStream("input.txt");
+const writeStream = fs.createWriteStream("output.txt");
+
+readStream.on("data", (chunk) => {
+    writeStream.write(chunk);
+});
+
+readStream.on("end", () => {
+    writeStream.end();
+});
