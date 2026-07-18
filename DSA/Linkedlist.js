9a845cc60852dccb301ea20f779f29c99b0f8ee8
Afroz Shaik <affrozali9342@gmail.com>
2026-07-18 12:04:31 +0530

    added  Remove List Elements

--- DSA/Linkedlist.js
+++ DSA/Linkedlist.js
@@ -18,3 +18,43 @@ while(current){
     console.log(current.data)
     current=current.next
 }
+
+
+// 2)  Remove Duplicates from Sorted List
+
+function removeDulp(head) {
+let current=head;
+
+while(current &&current.next){
+    let nextOne=current.next
+   if(current.val===nextOne.val){
+     current.next=nextOne.next;
+   }else{
+
+   current=nextOne
+   }
+}
+return head
+// return current
+
+
+};
+// 3). Remove Linked List Elements 
+function(head, val) {
+let dummy=new ListNode(0);
+dummy.next=head;
+let previous=dummy;
+let current=head;
+
+while(current){
+if(current.val===val){
+    previous.next=current.next;
+}
+else{
+    previous=current
+}
+current=current.next
+}
+
+return dummy.next 
+};
