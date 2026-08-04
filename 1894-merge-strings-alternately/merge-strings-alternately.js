0a5c4dcc67408bd037ec2ade46d80b48d06a4e2e
AfrozShaik <affrozali9342@gmail.com>
2026-08-04 07:04:55 +0530

    Time: 35 ms (94.93%) | Memory: 52.2 MB (98.75%) - LeetSync

---
+++ 1894-merge-strings-alternately/merge-strings-alternately.js
@@ -0,0 +1,25 @@
+/**
+ * @param {string} word1
+ * @param {string} word2
+ * @return {string}
+ */
+var mergeAlternately = function(word1, word2) {
+     
+     let i=0;
+     let j=0;
+     let str=""
+     while(i<word1.length&& j<word2.length){
+        str+=word1[i]+word2[j];
+        i++;
+        j++
+     }
+     while(i<word1.length){
+        str+=word1[i];
+        i++
+     }
+     while(j<word2.length){
+        str+=word2[j];
+        j++
+     }
+     return str
+};
