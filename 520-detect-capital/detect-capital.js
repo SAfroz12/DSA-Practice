e20c09dace548ccbc7217e00f361e8a1def11e14
AfrozShaik <affrozali9342@gmail.com>
2026-08-04 19:55:58 +0530

    Time: 0 ms (100.00%) | Memory: 55 MB (30.63%) - LeetSync

--- 520-detect-capital/detect-capital.js
+++ 520-detect-capital/detect-capital.js
@@ -2,19 +2,43 @@
  * @param {string} word
  * @return {boolean}
  */
-var detectCapitalUse = function(word) {
+var detectCapitalUse = function (word) {
 
-// USA - if caps ===usa.length  -- true
-//usa - if lows == usa.length = true
-//Usa - if caps ==1 && smallest==usa.length-1
+    // USA - if caps ===usa.length  -- true
+    //usa - if lows == usa.length = true
+    //Usa - if caps ==1 && smallest==usa.length-1
 
-if(word.toLowerCase() === word || word.toUpperCase() === word){
-    return true
-}else if(word[0].toUpperCase() + word.slice(1).toLowerCase() === word){
-    return true
-}else{
-    return false
-}
 
+
+    let Smallcount = 0
+    let Capcount = 0;
+
+    if (/[A-Z]/.test(word[0])) {
+        Capcount = Capcount + 1
+    }
+    else {
+        Smallcount = Smallcount + 1
+    }
+    for (let i = 1; i < word.length; i++) {
+        if (/[A-Z]/.test(word[i])) {
+            Capcount++
+        }
+        else {
+            Smallcount++
+        }
+
+    }
+    if (Capcount === 1 && /[A-Z]/.test(word[0])) {
+        return true
+    }
+    if (Capcount === word.length) {
+        return true
+    }
+     if (Smallcount === word.length) {
+        return true
+    }
+    else 
+        return false
+    
 
 };
