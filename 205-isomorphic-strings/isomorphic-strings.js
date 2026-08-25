d2fe353427a7c9590d9934fccbde9d3a1b17d673
AfrozShaik <affrozali9342@gmail.com>
2026-08-25 12:01:02 +0530

    Time: 5 ms (83.39%) | Memory: 55 MB (53.26%) - LeetSync

---
+++ 205-isomorphic-strings/isomorphic-strings.js
@@ -0,0 +1,21 @@
+/**
+ * @param {string} s
+ * @param {string} t
+ * @return {boolean}
+ */
+var isIsomorphic = function (s, t) {
+
+    if (s.length !== t.length) return false;
+    let map1 = new Map();
+    let map2 = new Map();
+    for (let i = 0; i < s.length; i++) {
+        map1.set(s[i], t[i]);
+        map2.set(t[i], s[i])
+    }
+  for(let i=0;i<s.length;i++){
+    if(map1.get(s[i])!==t[i] || map2.get(t[i])!==s[i]){
+        return false
+    }
+  }
+  return true
+};
