cb5a4771c60e05642a2c2906c33aa669d94e0a43
AfrozShaik <affrozali9342@gmail.com>
2026-09-03 10:26:26 +0530

    Time: 4 ms (75.93%) | Memory: 59.5 MB (36.22%) - LeetSync

---
+++ 125-valid-palindrome/valid-palindrome.js
@@ -0,0 +1,28 @@
+/**
+ * @param {string} s
+ * @return {boolean}
+ */
+var isPalindrome = function (s) {
+    let left = 0;
+    let right = s.length - 1;
+    while (left <= right) {
+
+
+
+        while (left <= right && !/[a-zA-Z0-9]/.test(s[left])) {
+            left++
+        }
+        while (left <= right && !/[a-zA-Z0-9]/.test(s[right])) {
+            right--
+        }
+
+        if (       left<right && s[left].toLowerCase() !== s[right].toLowerCase()) {
+            return false
+        } else {
+            left++;
+            right--
+        }
+
+    }
+    return true
+};
